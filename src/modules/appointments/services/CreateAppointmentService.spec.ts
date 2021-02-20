3e8fad489b19bc1f30d69c21a3b9f31a50f9b337
Francis Carvalho <inmob.comercial@gmail.com>
2021-02-20 10:18:16 -0300

    Testes e DDD - Criando so primeiros testes

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,23 @@
+import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  });
+
+  it('should be able to create a new appointment', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
