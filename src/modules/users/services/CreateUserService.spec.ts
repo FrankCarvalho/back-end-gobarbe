42c8c8275c4ff4401e18e9c5419d7bd630792845
Francis Carvalho <inmob.comercial@gmail.com>
2021-02-26 22:15:05 -0300

    Perfil de usuário - Refatoração dos testes

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -3,39 +3,34 @@ import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/Fakes/FakeHashProvider';
 import CreateUserService from './CreateUsersService';
 
-describe('CreateUser', () => {
-  it('should be able to create a new user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUser = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUser: CreateUserService;
 
+describe('CreateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+  });
+  it('should be able to create a new user', async () => {
     const user = await createUser.execute({
       name: 'Francis',
       email: 'francis@gmail.com',
       password: '123456',
     });
 
-    expect(user).toHaveProperty('id');
+    await expect(user).toHaveProperty('id');
   });
 
   it('should be able to create a new user with same email from another', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUser = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
     await createUser.execute({
       name: 'Francis',
       email: 'francis@gmail.com',
       password: '123456',
     });
 
-    expect(
+    await expect(
       createUser.execute({
         name: 'Francis',
         email: 'francis@gmail.com',
