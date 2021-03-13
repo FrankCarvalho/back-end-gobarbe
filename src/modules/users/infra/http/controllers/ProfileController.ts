cc1d1485576dc3562eb9f585121352ad38a3c996
Francis Carvalho <inmob.comercial@gmail.com>
2021-03-12 21:25:41 -0300

    Personalizando para produção - Validando dados,Variáveis ambiente,Utilizando Class Transformer

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
@@ -10,19 +11,9 @@ export default class ProfileController {
 
     const showProfile = container.resolve(ShowProfileService);
 
-    const user = await showProfile.execute({
-      user_id,
-    });
+    const user = await showProfile.execute({ user_id });
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.update_at,
-    };
-
-    return response.json(userWithoutPassword);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -39,15 +30,6 @@ export default class ProfileController {
       password,
     });
 
-    // Com a atualização do TypeScript, isso se faz necessário
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.update_at,
-    };
-
-    return response.json(userWithoutPassword);
+    return response.json(classToClass(user));
   }
 }
--- src/modules/users/infra/http/controllers/UsersControllers.ts
+++ src/modules/users/infra/http/controllers/UsersControllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
-
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
+
 import CreateUsersService from '@modules/users/services/CreateUsersService';
 
 export default class UsersControllers {
@@ -15,6 +16,6 @@ export default class UsersControllers {
       password,
     });
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
